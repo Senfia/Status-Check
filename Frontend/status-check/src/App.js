f2349f63984b84cf49b6c926f1201fd26f9c7c48
root <root@DESKTOP-JKHB9QO>
2023-10-26 12:20:53 +0000

    added profile image

--- Frontend/status-check/src/App.js
+++ Frontend/status-check/src/App.js
@@ -2,11 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from "react-bootstrap/esm/Button";
-import Image from "react-bootstrap/Image"
+/**import Image from "react-bootstrap/Image" **/
+import logo from './avatar.jpg'
+import NavLink from "react-bootstrap/esm/NavLink";
+
+const AVATAR = 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 function App(){
     return (
@@ -18,12 +21,14 @@ function App(){
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link href="#features">Feature</Nav.Link>
+                            <Nav.Link href="#pricing">Feature</Nav.Link>
                         </Nav>
                         <Nav>
                             <Button type="button">Dashboard</Button>
-                            <Image className="px-2" src="holder.js/171x180" roundedCircle />
+                            <NavLink className="font-weight-bold" href="/">
+                                <img src={AVATAR} alt="avatar" className="img-fluid rounded-circle" style={{ width: 30 }} />
+                            </NavLink>
                         </Nav>
                         </Navbar.Collapse>
                     </Container>
