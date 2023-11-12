958c0643fb233e1e2853f9e7b1ff6f414738e224
joseph darko <osomfonashticah@gmail.com>
2023-11-12 22:40:40 +0000

    chnges

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const { signup, signin } = require("../authentication/auth");
 const { addMonitor } = require("../controller/add_monitor");
+const Monitor = require("../model/monitor");
 
 const router = express.Router();
 
 router.post("/register", signup);
 router.post("/login", signin);
 router.post("/accounts", addMonitor);
+router.get("/accounts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const account = await Monitor.findById(id).populate("user");
+    console.log(account);
+    res.json({ account: account });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
